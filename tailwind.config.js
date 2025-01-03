c43c3ab799fd761e0544be5a4e6afaed7158ecfe
Den Cthulhu <cthulhu.den@gmail.com>
2025-01-03 01:07:14 +0100

    Fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts}'],
